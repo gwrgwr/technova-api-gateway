989afaa3089e62c991faa33a0fa6728a6b3f5b3f
gwrgwr <murilomassa72@gmail.com>
2025-04-17 13:06:09 -0300

    update: change private key location configuration in ApiGatewayApplication

--- src/main/java/com/technova/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/technova/apigateway/ApiGatewayApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-        @Value("${jwt.private-key-location:file:/app/keys/key.pem}")
+        @Value("${spring.jwt.keys.secret}")
         private static String privateKeyLocation;
     public static void main(String[] args) {
         System.out.println("Private Key Location: " + privateKeyLocation);
